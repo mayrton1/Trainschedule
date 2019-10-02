e658c09b3edc1842d818ebb029bf4b215de3e8e5
mayrton <ayrtonmatthew@yahoo.com>
2019-10-01 20:22:15 -0600

    added html fire base and javascript

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,102 +1,100 @@
-// Initialize Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyBh9wTpiEVr4i29KoM_Wfv-s-bpDgRxq9A",
-    authDomain: "creech-12.firebaseapp.com",
-    databaseURL: "https://creech-12.firebaseio.com",
-    projectId: "creech-12",
-    storageBucket: "creech-12.appspot.com",
-    messagingSenderId: "465248505243",
-    appId: "1:465248505243:web:6c53663447f5a28435253e"
+
+$(function (){
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCOjeRJLDCsw9Zut6Zp4TxYMnQ4y4BGH8A",
+    authDomain: "train-944c6.firebaseapp.com",
+    databaseURL: "https://train-944c6.firebaseio.com",
+    projectId: "train-944c6",
+    storageBucket: "",
+    messagingSenderId: "93391220962",
+    appId: "1:93391220962:web:36e0eeb936f8c1195c2ebb"
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-//Make var to call database
+
   var database = firebase.database();
   
-  // ---------STORAGE------------->
-
-//Var to push train names
+  
+  
+  
   var trainsName = "";
-//Var to push destination
+  
   var destination = "";
-//Var to push frequency
+  
   var freqMin = "";
-//Var to put next arrival time
+  
   var firstTrain = "";
-//Var to store time (in minutes)
+  
   var minsAway = "";
+  
+  
 
-  // ---------STORAGE------------->
-
-
-
-  // --------DATABASE PUSH--------->
-
-//OnClick submit button
+  //OnClick submit button
   $("#submit-button").on("click", function (e) {
-//Prevent repeats
+    //Prevent repeats
     e.preventDefault();
-
-    //the value of user input for trainName is = to the var ser up above same for destination freq and NA
+    
      trainsName = $("#trainNameInput").val();
-      destination = $("#destinationInput").val();
-      freqMin = $("#freqInput").val();
+     destination = $("#destinationInput").val();
+     freqMin = $("#freqInput").val();
       firstTrain = $("#firstTrainInput").val();
-
-    //push it all to the database
-     database.ref().push({
-          trainsName: trainsName,
+      
+      //push it all to the database
+      database.ref().push({
+        trainsName: trainsName,
           destination: destination,
           freqMin: freqMin,
           firstTrain: firstTrain
-     });
-     $("#trainNameInput").val("");
-     $("#destinationInput").val("");
+        });
+        $("#trainNameInput").val("");
+        $("#destinationInput").val("");
      $("#freqInput").val("");
      $("#firstTrainInput").val("");
   });
-
-  // --------DATABASE PUSH--------->
+  
 
 
   //function runs on new child added
   database.ref().on("child_added", function(data){
-      //make sure it works
-      console.log(data.val());
-
+    //make sure it works
+    console.log(data.val());
+    
       var tFrequency = data.val().freqMin
       var tFirstTrain = data.val().firstTrain
 
-      //split our hours and minutes apart from eachother and store into new array
+      
       var timeArr = tFirstTrain.split(":");
-    //  console.log(timeArr, "this is time array");
+   
       var tMinutes, tArrival;
       var firstTimeConverted = moment(tFirstTrain, "hh:mm").subtract(1, "years");
-    //  console.log(firstTimeConverted, "this is time converted");
+    
       var currentTime = moment();
-    //  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
+  
+      
       // Difference between the times
       var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    //  console.log("DIFFERENCE IN TIME: " + diffTime);
+      //  console.log("DIFFERENCE IN TIME: " + diffTime);
       var tRemainder = diffTime % tFrequency;
-    //  console.log(tRemainder,  "this is t remainder");
+      //  console.log(tRemainder,  "this is t remainder");
       var tMinutesTillTrain = tFrequency - tRemainder;
     //  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-      var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     //  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-      $("#TrainNames").append(data.val().trainsName + "<hr>")
+    $("#TrainNames").append(data.val().trainsName + "<hr>")
 
-     //get the val of destination and append it to the destination
-      $("#destination").append(data.val().destination + "<hr>")
+    //get the val of destination and append it to the destination
+    $("#destination").append(data.val().destination + "<hr>")
       $("#frequency").append(data.val().freqMin + "<hr>")
-
+      
       //get the val of nextArrival and append it to the NextArrival
       $("#firstTrain").append(data.val().firstTrain + "<hr>")
-
+      
       //get the val of minsaway and append it to The minsAway
       $("#minsAway").append(tMinutesTillTrain + "<hr>")
       //
       $("#arivalTime").append(moment(nextTrain).format("hh:mm") + "<hr>")
   });
+    })
